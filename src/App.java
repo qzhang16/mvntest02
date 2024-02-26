d63525e71e65d0683e56453fbc65d4b44b806a3a
qzhang16 <qzhang16.ra.rockwell.com>
2024-02-26 11:27:30 +0800

    test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       try {
+         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+   String aline;
+
+   aline = in.readLine();
+   System.out.println("Hello, World!" + aline);
+       } catch (Exception e) {
+        // TODO: handle exception
+       }
+        
     }
 }
